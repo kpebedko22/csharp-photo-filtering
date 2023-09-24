0104d3ea428e0d535c6ce74343b90a12d0547a6a
Alexander <36397871+kpebedko22@users.noreply.github.com>
2023-09-24 17:06:49 +0700

    Reformat median filter

--- MedianFilter.cs
+++ MedianFilter.cs
@@ -3,47 +3,51 @@ using System.Drawing;
 
 namespace PhotoFiltering {
 	class MedianFilter {
+		/// <summary>
+		/// Применения медианного фильтра.
+		/// Справляется с импульсными помехами.
+		/// </summary>
+		/// <param name="original"></param>
+		/// <param name="modified"></param>
+		/// <param name="matrixSize"></param>
+		public static void Apply(Bitmap original, ref Bitmap modified, int matrixSize) {
+			// Размер смещения
+			int ms = matrixSize / 2;
 
-		public static void Apply(Bitmap original, ref Bitmap modified, int matrix_size) {
-			/*
-			 * Медианный фильтр
-			 * Справляется с импульсными помехами
-			 */
-
-			// размер смещения
-			int ms = matrix_size / 2;
-
-			// размеры изображения
+			// Размеры изображения
 			int WIDTH = original.Width;
 			int HEIGHT = original.Height;
 
-			for (int y = 0; y < HEIGHT; y++)
+			for (int y = 0; y < HEIGHT; y++) {
 				for (int x = 0; x < WIDTH; x++) {
 
-					// сделать список пикселей и отсортировать его
+					// Собрать список пикселей и отсортировать его
 					List<int> pixels = new List<int>();
 
-					// в список попадают пиксели, находящиеся вокруг текущего
-					// собираются все пиксели которые находятся слева/справа/сверху/снизу и по диагоналям
-					// от текущего пикселя + в список добавляется сам текущий пиксель конечно же
-					for (int i = x - ms; i <= x + ms; i++)
+					// В список попадают пиксели, находящиеся вокруг текущего.
+					// Собираются все пиксели, которые находятся слева/справа/сверху/снизу и по диагоналям, 
+					// от текущего пикселя. В список добавляется текущий пиксель.
+					for (int i = x - ms; i <= x + ms; i++) {
 						for (int j = y - ms; j <= y + ms; j++) {
 
-							if (i < 0 || j < 0 || i > WIDTH - 1 || j > HEIGHT - 1)
+							if (i < 0 || j < 0 || i > WIDTH - 1 || j > HEIGHT - 1) {
 								continue;
+							}
+
 							pixels.Add(original.GetPixel(i, j).ToArgb());
 						}
+					}
 
-					// сортировка пикселей в списке
+					// Сортировка пикселей
 					pixels.Sort();
 
-					// индекс пикселя медианы
-					int median_index = (pixels.Count - 1) / 2;
+					// Индекс пикселя медианы
+					int medianIndex = (pixels.Count - 1) / 2;
 
-					// в результирующее изображение ставим найденный медианный пиксель
-					modified.SetPixel(x, y, Color.FromArgb(pixels[median_index]));
+					// В результирующее изображение устанавливается найденный медианный пиксель
+					modified.SetPixel(x, y, Color.FromArgb(pixels[medianIndex]));
 				}
+			}
 		}
-
 	}
 }
